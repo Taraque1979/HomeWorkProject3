b0bf78749c3992765eb55cb8cda8757ee8f62536
Taraque1979 <tstarafdar@gmail.com>
2023-09-18 17:43:30 -0400

    hw scanner

--- Home work Project3/src/hw6IfElseCondition01/Voter.java
+++ Home work Project3/src/hw6IfElseCondition01/Voter.java
@@ -2,34 +2,17 @@ package hw6IfElseCondition01;
 
 public class Voter {
 	public static void main(String[] args) {
-		int voterage =18;
-		int  myage =19;
-		
-		
-		
-		
-		if (voterage==myage){
-			System.out.println( "I am a voter" );
-		}else if (voterage>myage) {
+		int voterage = 18;
+		int myage = 19;
+
+		if (voterage == myage) {
+			System.out.println("I am a voter");
+		} else if (voterage > myage) {
 			System.out.println("I am a not voter");
-		}else if (voterage<myage) {
-				System.out.println("I am a voter from age 18");
+		} else if (voterage < myage) {
+			System.out.println("I am a voter from age 18");
 		}
-			
-		}
-		
+
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
